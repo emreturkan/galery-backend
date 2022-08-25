4bae613b206e5a42b3c6b7367f2b4da0df95ccd0
emre turkan <emreturkan10@gmail.com>
2022-08-25 19:44:19 +0300

    Added a Procfile.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
+const xss = require('xss-clean');
 const connectDB = require('./db/connectDB.js')
 const router = require('./routes/galeryRouter.js')
 const cloudinary = require('cloudinary').v2
@@ -12,8 +13,15 @@ cloudinary.config({
 })
 const app = express()
 app.use(express.json())
-app.use(cors())
 app.use(fileUpload({useTempFiles:true}))
+app.use(cors())
+app.use(xss())
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Galery API</h1>');
+  });
+
 app.use('/api/v1/',router)
 
 
